07c7e4f42757f02ae8de06696d3e67a5607dc283
Ciaran McGoldrick <cmcgoldrick@awesemo.com>
2020-04-08 17:13:53 -0500

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lethal.Developer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lethal.Developer.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Topic> Topics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Topic>(topic =>
+            {
+                //TODO: need to determine the right way for setting up these relationships
+
+                //topic
+                //    .Property(n => n.UserId).IsRequired();
+
+                topic.HasData(
+                    new Topic { Id = 1, Name = "Data Structures" }
+                    );
+            });
+        }
+
+    }
+}
---
+++ Models/Topic.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lethal.Developer.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public Guid UserId { get; set; }
+
+        public virtual IdentityUser User { get; set; }
+    }
+}
